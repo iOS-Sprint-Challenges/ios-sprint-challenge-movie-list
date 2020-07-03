056ccc13af754df8a50b98a42a067fd936af256f
fritzgt <tuanis@live.com>
2020-07-03 14:03:01 +0300

    create movie model

---
+++ Movie List/Model/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  Movie List
+//
+//  Created by FGT MAC on 7/3/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Movie {
+    var name: String
+    var watched: Bool
+}
