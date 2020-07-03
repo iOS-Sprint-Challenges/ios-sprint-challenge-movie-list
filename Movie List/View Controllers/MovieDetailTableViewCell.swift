cf85eb073ea77c19f2684eae8dabc6cf67f3339e
fritzgt <tuanis@live.com>
2020-07-03 14:19:55 +0300

    created view controllers

---
+++ Movie List/View Controllers/MovieDetailTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieDetailTableViewCell.swift
+//  Movie List
+//
+//  Created by FGT MAC on 7/3/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
